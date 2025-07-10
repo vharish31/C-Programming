adcc7c2ce56059b3d860c9f94cf64a14f3bf3bcc
vharish31 <harishhvp31@gmail.com>
2025-07-10 13:45:42 +0530

    first commit

--- Strings/Length.c
+++ Strings/Length.c
@@ -1,2 +1,18 @@
 // To find lenght of the string
 
+#include<stdio.h>
+int main(){
+    char string[1000];
+    int len=0;
+    printf("Enter a string : ");
+    scanf("%s",&string);
+
+    while(string[len]>0){
+        len++;
+    }
+    
+    printf("Length of string is %d",len);
+
+    return 0;
+
+}
